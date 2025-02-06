156c8d5697a8196b1468428bf90f21cfa9f2b963
Sukhrattan Singh <150605238+Sukhrattan@users.noreply.github.com>
2025-02-06 22:04:28 +0000

    Solution to Check Positive or Negative

--- Check Positive or Negative@@6788dd63490637cfc44e1b8f/code.c
+++ Check Positive or Negative@@6788dd63490637cfc44e1b8f/code.c
@@ -2,7 +2,7 @@
 int main() {
     int num;
     scanf("%d",&num);
-    if(num&(1<<31)){
+    if(num&(1<<31)){ //checking if MSB is 1 or not , since if MSB is 1 it means the integer is negative according to DECA
         printf("Negative");
     }
     else if(num==0) {
