fd4b0f6fb30013a4963db3e17d59144a0490a99c
Sukhrattan Singh <150605238+Sukhrattan@users.noreply.github.com>
2025-02-11 16:38:52 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main (void){
-    int days[12] = {0,31,28,31,30,31,30,31,30,31,30,31,30};
+    int *days[] = {0,31,28,31,30,31,30,31,30,31,30,31,30};
     int inp;
     scanf("%d",&inp);
     if(inp>=1 && inp<=12){
